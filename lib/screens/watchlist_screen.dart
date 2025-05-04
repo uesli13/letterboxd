e972bf548df62eb08f929348241cb80fdb5ae93d
uesli13 <pan.lekos@gmail.com>
2025-05-04 19:04:54 +0100

    Implement Slidable functionality in WatchedScreen and WatchlistScreen for movie removal

--- lib/screens/watchlist_screen.dart
+++ lib/screens/watchlist_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../services/data_service.dart';
 import '../widgets/movie_card.dart';
 import 'movie_detail_screen.dart';
@@ -30,20 +31,40 @@ class _WatchlistScreenState extends State<WatchlistScreen> {
               itemCount: watchlistMovies.length,
               itemBuilder: (context, index) {
                 final movie = watchlistMovies[index];
-                return MovieCard(
-                  movie: movie,
-                  onTap: () async {
-                    // Navigate to MovieDetailScreen and wait for the result
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MovieDetailScreen(movie: movie),
+                return Slidable(
+                  key: ValueKey(movie.title), // Unique key for each movie
+                  endActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          setState(() {
+                            DataService.user.watchlist.remove(movie);
+                          });
+                          DataService.saveData(); // Save changes to persistent storage
+                        },
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Remove',
                       ),
-                    );
+                    ],
+                  ),
+                  child: MovieCard(
+                    movie: movie,
+                    onTap: () async {
+                      // Navigate to MovieDetailScreen and wait for the result
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDetailScreen(movie: movie),
+                        ),
+                      );
 
-                    // Refresh the state when returning from MovieDetailScreen
-                    setState(() {});
-                  },
+                      // Refresh the state when returning from MovieDetailScreen
+                      setState(() {});
+                    },
+                  ),
                 );
               },
             ),
